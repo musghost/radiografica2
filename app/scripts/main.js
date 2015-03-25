79d1862515964451707100f48f21718b9bb35d37
Andrés Cidel <ac@manoderecha.mx>
2015-03-24 20:33:31 -0600

    Add projects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,62 @@
 'use strict';
-$('.player').YTPlayer();
 
-$('#description').scrollspy({
-  min: $('#description').offset().top - 300,
-  max: $('#description').offset().top + 300,
-  onEnter: function(element){
-    $(element).addClass('fadeina');
-  }
-});
+(function(){
+  $('.player').YTPlayer();
 
-$('#projects').scrollspy({
-  min: $('#projects').offset().top - 300,
-  max: $('#projects').offset().top + 300,
-  onEnter: function(element){
-    $(element).addClass('active');
+  if($('#description').length !== 0){
+    $('#description').scrollspy({
+      min: $('#description').offset().top - 300,
+      max: $('#description').offset().top + 300,
+      onEnter: function(element){
+        $(element).addClass('fadeina');
+      }
+    });
   }
-});
 
-$('#meet-us').scrollspy({
-  min: $('#meet-us').offset().top - 400,
-  max: $('#meet-us').offset().top + 300,
-  onEnter: function(element){
-    $(element).addClass('fadeina');
+  if($('#projects').length !== 0){
+
+    $('#projects').scrollspy({
+      min: $('#projects').offset().top - 300,
+      max: $('#projects').offset().top + 300,
+      onEnter: function(element){
+        $(element).addClass('active');
+      }
+    });
   }
-});
+
+  if($('#meet-us').length !== 0){
+
+    $('#meet-us').scrollspy({
+      min: $('#meet-us').offset().top - 400,
+      max: $('#meet-us').offset().top + 300,
+      onEnter: function(element){
+        $(element).addClass('fadeina');
+      }
+    });
+  }
+})();
+
+(function(){
+  $('#send-form').submit(function(a){
+    a.preventDefault();
+    var form = $('#form');
+    var message = $('#message').val();
+    $('#message').val('');
+    $(this).find('button').attr('disabled', 'disabled');
+    $.ajax({
+      type: 'POST',
+      data: 'message=' + message,
+      url: 'mail.php',
+      success: function(msg){
+        if(msg === 'ok'){
+          form.find('.success').css('display', 'inline-block');
+        } else {
+          form.find('.error').css('display', 'inline-block');
+        }
+      },
+      error: function(){
+        form.find('.error').css('display', 'inline-block');
+      }
+    });
+  });
+})();
